f6b38604ba05ca639e7713d298f36b490d10a011
KasjanK <kowalczykkasjan@gmail.com>
2025-11-04 20:52:37 +0100

    feat: add a feed and connect it to your user

---
+++ handler_addFeed.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/KasjanK/blog-aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.Arguments) != 2 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	feedName := cmd.Arguments[0]
+	feedUrl := cmd.Arguments[1]
+	currentUser, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("couldn't get user: %w", err)
+	}
+	
+	feed, err := s.db.CreateFeed(context.Background(), database.CreateFeedParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now(),	
+		UpdatedAt: time.Now(),
+		Name: feedName,
+		Url: feedUrl,
+		UserID: currentUser.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("error creating feed: %w", err)
+	}
+
+	fmt.Println("Feed created successfully!")
+	printFeed(feed)
+	fmt.Println()
+	fmt.Println("=====================================")
+	return nil
+}
+
+func printFeed(feed database.Feed) {
+	fmt.Printf("* ID:            %s\n", feed.ID)
+	fmt.Printf("* Created:       %v\n", feed.CreatedAt)
+	fmt.Printf("* Updated:       %v\n", feed.UpdatedAt)
+	fmt.Printf("* Name:          %s\n", feed.Name)
+	fmt.Printf("* URL:           %s\n", feed.Url)
+	fmt.Printf("* UserID:        %s\n", feed.UserID)
+}
--- handler_user.go
+++ handler_user.go
@@ -36,26 +36,24 @@ func handlerRegister(s *state, cmd command) error {
 	}
 
 	name := cmd.Arguments[0] 
-	newUser, err := s.db.CreateUser(context.Background(), 
-	database.CreateUserParams{
+	newUser, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
 		ID: uuid.New(),
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 		Name: name,
-	},
-)
-if err != nil {
-	return fmt.Errorf("could not create user: %w", err)
-}
+	})
+	if err != nil {
+		return fmt.Errorf("could not create user: %w", err)
+	}
 
-err = s.cfg.SetUser(newUser.Name)
-if err != nil {
-	return fmt.Errorf("could not set current user: %w", err)
-}
+	err = s.cfg.SetUser(newUser.Name)
+	if err != nil {
+		return fmt.Errorf("could not set current user: %w", err)
+	}
 
-fmt.Printf("user was created. ID: %d, created at: %s, updated at: %s, name: %s", newUser.ID, newUser.CreatedAt, newUser.UpdatedAt, newUser.Name)
+	fmt.Printf("user was created. ID: %d, created at: %s, updated at: %s, name: %s", newUser.ID, newUser.CreatedAt, newUser.UpdatedAt, newUser.Name)
 
-return nil
+	return nil
 }
 
 func handlerUsers(s *state, cmd command) error {
